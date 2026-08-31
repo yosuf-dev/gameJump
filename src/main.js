c757202a52a6df54f9c7b596cae7ba9549b96485
yosuf saleh zadeh <yosuf.saleh.zadeh@gmail.com>
2026-08-31 16:43:43 +0330

    update bog

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import Welcome from "./pages/welcome.vue";
-import Game from "./pages/game.vue";
-import Result from "./pages/result.vue";
+import Welcome from "./pages/Welcome.vue";
+import Game from "./pages/Game.vue";
+import Result from "./pages/Result.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
