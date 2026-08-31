a4fd0e52af30362cbe1dec4519db5815c3f5e877
yosuf-dev <yosuf.saleh.zadeh@gmail.com>
2026-08-31 16:27:18 +0330

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()],
-<<<<<<< HEAD
-  base: '/gameJump/'
-})
-=======
-  base: "/gameJump/",
-
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-})
->>>>>>> 7888d51 (Deploy)
+    plugins: [vue()],
+    base: "/gameJump/",
+});
