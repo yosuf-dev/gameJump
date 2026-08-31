6be4adbaf0e2857fc1729ae1bba84e8c8eebe814
yosuf saleh zadeh <yosuf.saleh.zadeh@gmail.com>
2026-08-31 16:55:45 +0330

    bog 2

--- src/main.js
+++ src/main.js
@@ -1,22 +1,36 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+
 import Welcome from "./pages/Welcome.vue";
 import Game from "./pages/Game.vue";
 import Result from "./pages/Result.vue";
-import { createRouter, createWebHistory } from "vue-router";
+
+import {
+  createRouter,
+  createWebHashHistory,
+} from "vue-router";
 
 const routes = [
-  { path: "/", component: Welcome },
-  { path: "/game", component: Game },
-  { path: "/result", component: Result },
+  {
+    path: "/",
+    component: Welcome,
+  },
+  {
+    path: "/game",
+    component: Game,
+  },
+  {
+    path: "/result",
+    component: Result,
+  },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
 createApp(App)
-.use(router)
-.mount("#app");
+  .use(router)
+  .mount("#app");
