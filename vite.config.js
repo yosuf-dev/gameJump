36d46b6812317eff8e6170122860f816c330525a
yosuf saleh zadeh <yosuf.saleh.zadeh@gmail.com>
2026-08-31 16:51:12 +0330

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/gameJump/",
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
+  base: '/gameJump/'
 })
